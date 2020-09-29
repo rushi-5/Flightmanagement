17f8bed796e46322a8e3f4f056411946108ee5a8
rushi-5 <61549269+rushi-5@users.noreply.github.com>
2020-09-29 12:01:37 +0530

    Add files via upload

---
+++ welcomeuser/welcomeuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcomeuser',
+  templateUrl: './welcomeuser.component.html',
+  styleUrls: ['./welcomeuser.component.css']
+})
+export class WelcomeuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
